32f8f936d2fea2dc34978205bea02e7bb3123dd0
Bill White <soniclinuxknight@hotmail.com>
2021-08-14 02:03:57 -0500

    Initial Commit

---
+++ components/index.jsx
@@ -0,0 +1,6 @@
+export { default as Header } from './Header/Header';
+export { default as Banner } from './Banner/Banner';
+export { default as SmallCard } from './SmallCard/SmallCard';
+export { default as MediumCard } from './MediumCard/MediumCard';
+export { default as LargeCard } from './LargeCard/LargeCard';
+export { default as Footer } from './Footer/Footer';
