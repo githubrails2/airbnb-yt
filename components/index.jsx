f2b9a61cf4efd902c9a24c3a710d86d9ccf5ad1f
Bill White <soniclinuxknight@hotmail.com>
2021-08-15 10:09:13 -0500

    Add InfoCard and progress bar

--- components/index.jsx
+++ components/index.jsx
@@ -4,3 +4,4 @@ export { default as SmallCard } from './SmallCard/SmallCard';
 export { default as MediumCard } from './MediumCard/MediumCard';
 export { default as LargeCard } from './LargeCard/LargeCard';
 export { default as Footer } from './Footer/Footer';
+export { default as InfoCard } from './InfoCard/InfoCard';
